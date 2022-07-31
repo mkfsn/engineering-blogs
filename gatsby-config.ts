6cab75c22a6960e9935d886b982aeb97477062c4
Pei-Ming Wu <p408865@gmail.com>
2022-07-31 20:39:19 +0800

    feat: deploy to github pages

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -3,8 +3,9 @@ import type { GatsbyConfig } from "gatsby";
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `Engineering Blogs`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://mkfsn.github.io/`
   },
+  pathPrefix: "engineering-blogs",
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
